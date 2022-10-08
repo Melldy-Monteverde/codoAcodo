b429f390ae555d492c85bad62a19ebe262c63958
Melldy Monteverde <monteverdelex@gmail.com>
2022-10-08 19:18:16 -0300

    agrega galeria, animaciones y estilos generales

--- README.md
+++ README.md
@@ -5,16 +5,22 @@ Perichón - Articulos de limpieza textiles.
 **Secciones:**
 
 - Home (en progreso)
-- Contacts (en progreso)
+- Nosotros (en progreso)
+- Contactos (en progreso)
 - [Instagram](https://www.instagram.com/trapos_perichon/)
 - [Facebook](https://www.facebook.com/traposperichon)
+
+**Animaciones**
+- navbar
+- hero title
 
 **[Dashboard del proyecto](https://trello.com/b/SCJv9Mmy/codo-a-codo-workspace)**
 ## Autores
 
 ---
 
-- Federico Gomez:
+- [Federico Gomez](https://www.linkedin.com/in/federico-gomez-gigliani/):
+  +[github](https://github.com/federicoggigliani)
 
 - [Melldy Montevede](https://www.linkedin.com/in/melldy-monteverde-b19390190/) 
   + [github](https://github.com/Melldy-Monteverde)
--- js/app.js
+++ js/app.js
@@ -1,1 +1,35 @@
-console.log('Hola coder!')
+let form = document.getElementById("my-form");
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  let status = document.getElementById("my-form-status");
+  let data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        status.innerHTML = "Gracias por contactarnos!";
+        form.reset();
+      } else {
+        response.json().then((data) => {
+          if (Object.hasOwn(data, "errors")) {
+            status.innerHTML = data["errors"]
+              .map((error) => error["message"])
+              .join(", ");
+          } else {
+            status.innerHTML = "Oops! Hubo un problema enviando tu mensaje.";
+          }
+        });
+      }
+    })
+    .catch((error) => {
+      status.innerHTML = "Oops! Hubo un problema enviando tu mensaje.";
+    });
+}
+
+form.addEventListener("submit", handleSubmit);
